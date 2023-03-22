9cb2c673d7849abbe6a5f93af559104f39a8574c
Phuc TP <truongphuc246@gmail.com>
2023-03-22 15:59:24 +0700

    Login and Register design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,68 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Router,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register"
+import Login from "./pages/Login";
+import Single from "./pages/Single"; 
+import Write from "./pages/Write";  
+import Home from "./pages/Home"; 
+import Navbar from "./components/Navbar"; 
+import Footer from "./components/Footer";
+import "./style.scss"
+const Layout = () =>{
+  return(
+    <>
+      <Navbar/>
+      <Outlet/> 
+      <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>, 
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/login/:id",
+        element: <Single/>,
+      },
+      {
+        path: "/write",
+        element: <Write/>,
+      },  {
+        path: "/register/:id",
+        element: <Register/>,
+      },
+
+    ]
+      
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
+
 
 export default App;
